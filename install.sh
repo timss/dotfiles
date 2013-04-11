d93e85dd1b7f6f32be7479d797889370adbeaf53
timss <timsateroy@gmail.com>
2013-04-11 19:55:24 +0200

    Small update, but still shit.

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@
 set -e
 
 # Should be more elegant in the future...
+# More to come. Maybe.
 ln -s "bashrc" "$HOME/.bashrc"
 ln -s "alias" "$HOME/.alias"
+ln -s "gitconfig" "$HOME/.gitconfig"
+ln -s "gitignore_global" "$HOME/.gitignore_global"
 ln -s "tmux.conf" "$HOME/.tmux.conf"
