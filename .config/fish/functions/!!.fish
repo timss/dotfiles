da7ad6d187984f0c30480272bd3bc358bb39329a
timss <timsateroy@gmail.com>
2016-01-26 15:52:54 +0100

    Fix "!!" and "sudo !!".

--- .config/fish/functions/!!.fish
+++ .config/fish/functions/!!.fish
@@ -1,4 +1,9 @@
 # https://gist.github.com/b-/981892a65837ab0a387e
 function !! --description "Run last command"
-    eval $history[1] $argv;
+    if not test $history[1] = "!!"
+        eval $history[1] $argv;
+    else
+        # repeated !! will be deduplicated in $history
+        eval $history[2] $argv;
+    end
 end
--- .config/fish/functions/sudo.fish
+++ .config/fish/functions/sudo.fish
@@ -1,9 +1,14 @@
 # history expasion; run last command
 # https://gist.github.com/b-/981892a65837ab0a387e
 function sudo --description "Execute a command as another user"
-    if test $argv[1] = "!!"
-        command sudo !!;
+    if test $argv
+        if test $argv[1] = "!!"
+            # evade recursive sudo function
+            eval /usr/bin/sudo $history[1];
+        else
+            command sudo $argv;
+        end
     else
-        command sudo $argv;
+        command sudo;
     end
-end 
+end
