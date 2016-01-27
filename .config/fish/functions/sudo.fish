2e1a1f61b496d75d815ff294d9e76149610499cd
timss <timsateroy@gmail.com>
2016-01-27 09:22:28 +0100

    Fix testing for arguments:
    If $argv is not wrapped in "" or similar, it may break for commands like:
    
        sudo umount -l dir1 dir2 dir3
    
    This is because `-l` will be taken into consideration.
    A better approach is using command substition and checking the length.

--- .config/fish/functions/sudo.fish
+++ .config/fish/functions/sudo.fish
@@ -1,7 +1,7 @@
 # history expasion; run last command
 # https://gist.github.com/b-/981892a65837ab0a387e
 function sudo --description "Execute a command as another user"
-    if test $argv
+    if test (count $argv) -gt 0
         if test $argv[1] = "!!"
             # evade recursive sudo function
             eval /usr/bin/sudo $history[1];
