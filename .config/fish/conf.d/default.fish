bdafe3e2ed2cd367be8fe9cd9c901b11534ec15b
timss <timsateroy@gmail.com>
2023-08-10 22:52:41 +0200

    Ensure sbin and user's bin directories are added to `$PATH`

--- .config/fish/conf.d/default.fish
+++ .config/fish/conf.d/default.fish
@@ -2,3 +2,9 @@
 set fish_greeting
 
 set -gx EDITOR (which vim)
+
+# If necessary delete using `set -e fish_user_paths[index]`
+fish_add_path /sbin
+fish_add_path /usr/sbin
+fish_add_path /usr/local/sbin
+fish_add_path $HOME/bin
